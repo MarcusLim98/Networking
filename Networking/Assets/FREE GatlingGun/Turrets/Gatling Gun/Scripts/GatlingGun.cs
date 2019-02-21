817a16891f8e16baa93ace80dc9f08e077fa2d6c
dygold <44193932+dygold@users.noreply.github.com>
2019-02-21 23:57:30 +0800

    Gun FOV limit, cooldown
    Rotation is hard and I spent hours trying to do it. I failed.

--- Networking/Assets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
+++ Networking/Assets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
@@ -18,34 +18,46 @@ public class GatlingGun : MonoBehaviour
     public ParticleSystem muzzelFlash;
 
     // Used to start and stop the turret firing
-    bool canFire = false;
+    bool canFire = false, overheated, inSight;
 
     //VRTK objects
     public VRTK_ControllerEvents leftCtrller, rightCtrller;
     public GameObject headsetObj;
 
+    float gunTimer = 20f, coolDown;
+
+    public bool isFiring;
+
 
     void Update()
     {
 
-        if (leftCtrller.triggerClicked && rightCtrller.triggerClicked || Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.X))
+        if (leftCtrller.triggerPressed && rightCtrller.triggerPressed || Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.X))
         {
             canFire = true;
-        }  else { canFire = false; }
+        }
+        else { canFire = false; }
 
-        AimAndFire();
+        FireEffects();
     }
 
-    void AimAndFire()
+    void FireEffects()
     {
         // Gun barrel rotation
         go_barrel.transform.Rotate(0, 0, currentRotationSpeed * Time.deltaTime);
 
         // if can fire turret activates
-        if (canFire)
+        if (canFire && !overheated && inSight)
         {
             // start rotation
             currentRotationSpeed = barrelRotationSpeed;
+
+            gunTimer -= Time.deltaTime;
+            if (gunTimer < 0)
+            {
+                overheated = true;
+                coolDown = 0f;
+            }
 
             // start particle system 
             if (!muzzelFlash.isPlaying)
@@ -57,6 +69,13 @@ public class GatlingGun : MonoBehaviour
         {
             // slow down barrel rotation and stop
             currentRotationSpeed = Mathf.Lerp(currentRotationSpeed, 0, 10 * Time.deltaTime);
+            
+            coolDown += Time.deltaTime;
+            if (coolDown > 5f)
+            {
+                gunTimer = 20f;
+                overheated = false;
+            }
 
             // stop the particle system
             if (muzzelFlash.isPlaying)
@@ -74,12 +93,22 @@ public class GatlingGun : MonoBehaviour
         {
             Debug.DrawRay(headsetObj.transform.position, headsetObj.transform.TransformDirection(transform.forward) * hit.distance, Color.red);
 
-            //turns head to face where player is looking
-            Vector3 baseTargetPostition = new Vector3(hit.point.x, this.transform.position.y, hit.point.z);
-            Vector3 gunBodyTargetPostition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            Vector3 targetDir = hit.point - transform.position;
+            float angleToPlayer = Vector3.Angle(targetDir, transform.forward);
 
-            go_baseRotation.transform.LookAt(baseTargetPostition);
-            go_GunBody.transform.LookAt(gunBodyTargetPostition);
+            if (angleToPlayer >= -75 && angleToPlayer <= 75)    //limited 180 degree FOV
+            {
+
+                //turns head to face where player is looking
+                Vector3 baseTargetPostition = new Vector3(hit.point.x, this.transform.position.y, hit.point.z);
+                Vector3 gunBodyTargetPostition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+
+                go_baseRotation.transform.LookAt(baseTargetPostition);
+                go_GunBody.transform.LookAt(gunBodyTargetPostition);
+
+                inSight = true;
+            }
+            else { inSight = false; }
         }
     }
 }
---
+++ Networking/Assets/Scripts/ShootingController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour
+{
+
+    GatlingGun gunScript;
+    //EnemyCtrl enemy;
+    int shotDmg;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (gunScript.isFiring)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //RaycastHit hit;
+        //if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), out hit, Mathf.Infinity))
+        //{
+        //    if (hit.collider != null && hit.collider.GetComponent<EnemyCtrl>())
+        //    {
+        //        enemy = hit.collider.GetComponent<EnemyCtrl>();
+        //        enemy.GetDamaged(shotDmg);
+        //    }
+        //    else return;
+        //}
+    }
+}
