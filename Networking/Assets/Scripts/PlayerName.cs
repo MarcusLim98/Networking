127d0b57269cf4f9c941e07e503bb560c38fcffb
dygold <44193932+dygold@users.noreply.github.com>
2019-02-28 07:38:28 +0800

    Name?

---
+++ Networking/Assets/Scripts/PlayerName.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+
+    InputField nameInput;
+    public string savedName = "player";
+
+    void Start()
+    {
+        nameInput = GameObject.Find("NameInput").GetComponent<InputField>();
+        RegisterName();
+    }
+
+    public void RegisterName()
+    {
+        if (nameInput.text != null)
+        {
+            savedName = nameInput.text;
+        }
+        else
+        {
+            savedName = "player";
+        }
+    }
+}
--- Networking/Assets/Scripts/SetupLocalPlayer.cs
+++ Networking/Assets/Scripts/SetupLocalPlayer.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class SetupLocalPlayer : NetworkBehaviour
 {
-    // Start is called before the first frame update
+
+    public Text namePrefab, nameLabel;
+    public Transform namePos;
+    string textBoxName = "";
+
+    PlayerName nameScript;
+
+    [SyncVar(hook = "OnChangeName")]
+    public string pName = "player";
+
+    void OnChangeName(string n)
+    {
+        pName = n;
+        nameLabel.text = pName;
+    }
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        pName = newName;
+        nameLabel.text = pName;
+    }
+
     void Start()
     {
+        nameScript = GameObject.Find("NetworkManager").GetComponent<PlayerName>();
+
+        GameObject canvas = GameObject.FindGameObjectWithTag("MainCanvas");
+        nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
+        nameLabel.transform.SetParent(canvas.transform);
+
         if (isLocalPlayer)
         {
             GetComponent<PlayerLogic>().enabled = true;
+
+            CmdChangeName(nameScript.savedName);
         }
         else
         {
@@ -18,9 +49,9 @@ public class SetupLocalPlayer : NetworkBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+        nameLabel.transform.position = nameLabelPos;
     }
 }
