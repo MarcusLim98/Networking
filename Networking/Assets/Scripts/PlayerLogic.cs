bc84d0249f1022fb8fd76cc83a3ad8f8c54c463a
Marcus Lim <marcus.haloODST@gmail.com>
2019-02-28 02:34:56 +0800

    Player movement tweaks

--- Networking/Assets/Scripts/PlayerLogic.cs
+++ Networking/Assets/Scripts/PlayerLogic.cs
@@ -7,36 +7,63 @@ public class PlayerLogic : MonoBehaviour
     public float runSpeed, jumpSpeed;
     protected Joystick js;
     protected JoyButton jb;
-    bool jump, canJump = true;
+    float xzAxis, yAxis;
+    bool jump, canJump = true, canMove = false;
     Rigidbody rb;
+    Animator anim;
+    float oldPosX;
     // Start is called before the first frame update
     void Start()
     {
         js = FindObjectOfType<Joystick>();
         jb = FindObjectOfType<JoyButton>();
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(js.Horizontal * runSpeed, rb.velocity.y, js.Vertical * runSpeed);
+        rb.velocity = new Vector3(js.Horizontal * xzAxis, rb.velocity.y, js.Vertical * xzAxis);
 
         Vector3 lookDirection = new Vector3(js.Horizontal, 0, js.Vertical );
-        transform.rotation = Quaternion.LookRotation(lookDirection);
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, lookDirection, 3f * Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+
+        if (rb.velocity.magnitude < 0)
+        {
+            anim.SetInteger("State", 1);
+        }
+        else if (rb.velocity.magnitude > 0)
+        {
+            anim.SetInteger("State", 2);
+        }
 
         if (canJump)
         {
             if (!jump && jb.pressed)
             {
                 jump = true;
-                rb.velocity += Vector3.up * jumpSpeed;
+                rb.velocity += Vector3.up * yAxis;
             }
             else if (jump && jb.pressed)
             {
                 jump = false;
             }
         }
+    }
+
+    public void StopMove()
+    {
+        xzAxis = 0;
+        yAxis = 0;
+    }
+
+    public void CanMove()
+    {
+        anim.SetInteger("State", 1);
+        xzAxis = runSpeed;
+        yAxis = jumpSpeed;
     }
 
     private void OnCollisionEnter(Collision collision)
