7185b0cf043bbc55206dfeee409701b97c2ac1e2
Marcus Lim <marcus.haloODST@gmail.com>
2019-02-28 03:30:56 +0800

    Networking tweaks

---
+++ Networking/Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<PlayerLogic>().enabled = true;
+        }
+        else
+        {
+            GetComponent<PlayerLogic>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
